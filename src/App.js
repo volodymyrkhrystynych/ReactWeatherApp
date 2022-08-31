b3ac5b4865a2e351e23b8241982deb95bf26cc74
Volodymyr <v.khryst@outlook.com>
2022-08-31 12:54:30 -0400

    now properly displays the different city pictures

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import WeatherData from './WeatherData.js';
 import Location from './Location/Location.js';
 import Cities from './cities/cities.js';
 import NYC from './cities/nyc.jpg';
+import Tok from './cities/tokyo.jpg';
+import london from './cities/london.jpg';
 
 function App() {
   const API_key = 'fc38166d48d39f532d2ad0e8dde12273'
@@ -21,7 +23,13 @@ function App() {
         setCity(acity); 
         setLon(longitude);
         setLat(latitude);
-        setImglink(imglink);
+        if (acity == "New York"){
+          setImglink(NYC);
+        } else if (acity == "Tokyo"){
+          setImglink(Tok);
+        } else if (acity == "London"){
+          setImglink(london);
+        }
     }
 
 
@@ -30,7 +38,7 @@ function App() {
       <Location lon={lon} lat={lat} setLon={setLon} setLat={setLat}/>
       <WeatherData lon={lon} lat={lat} apiKey={API_key}/>
       <Cities onClick={handleClick} />
-      {(city != "") && <img src={NYC} style={{height:"400px",width:"400px"}}/>}
+      {(imgLink != "") && <img src={imgLink} alt="CityImage" style={{height:"400px",width:"400px"}}/>}
     </div>
   );
 }
